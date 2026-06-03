b018808c1111ac1eb5dffe540d6beb8b0b3685a2
Ajay MS <ajay.ms2000@gmail.com>
2026-06-03 16:00:07 +0530

    Shared Kernel

---
+++ src/MOR.SharedKernel/Text/Json/Date Converters.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Json.Serialization;
+
+namespace System.Text.Json
+{
+    public class JsonConverterDateTime : JsonConverter<DateTime>
+    {
+        public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            var ret = reader.GetDateTime();
+            return ret;
+        }
+
+        public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.ToString("s"));
+        }
+    }
+
+    public class JsonConverterDateTimeOffset : JsonConverter<DateTimeOffset>
+    {
+        public override DateTimeOffset Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            var ret = reader.GetDateTimeOffset();
+            return ret;
+        }
+
+        public override void Write(Utf8JsonWriter writer, DateTimeOffset value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.ToString("s") + value.ToString("%K"));
+        }
+    }
+}
