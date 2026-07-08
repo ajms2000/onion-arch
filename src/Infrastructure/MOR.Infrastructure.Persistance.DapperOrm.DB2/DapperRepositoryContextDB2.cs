d7b62b9f6c36e378e60079ae91aa85ad3e403e93
Ajay MS <ajay.ms2000@gmail.com>
2026-07-09 00:53:12 +0530

    AA

---
+++ src/Infrastructure/MOR.Infrastructure.Persistance.DapperOrm.DB2/DapperRepositoryContextDB2.cs
@@ -0,0 +1,17 @@
+﻿using IBM.Data.Db2;
+
+namespace MOR.Infrastructure.Persistance.DapperOrm.DB2
+{
+    public class DapperRepositoryContextDB2 : DapperRepositoryContextBase<DB2Connection, DB2Transaction>
+    {
+        public DapperRepositoryContextDB2(string connectionString)
+            : base(connectionString)
+        {
+        }
+
+        protected override DB2Connection CreateConnectionObject()
+        {
+            return new DB2Connection(ConnectionString);
+        }
+    }
+}
