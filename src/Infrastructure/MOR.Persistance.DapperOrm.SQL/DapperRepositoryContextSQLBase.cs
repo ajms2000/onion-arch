70a537f2b10ef4c48d13135f01317ddca3014cb6
Ajay MS <ajay.ms2000@gmail.com>
2026-07-10 00:29:44 +0530

    AAA

--- src/Infrastructure/MOR.Persistance.DapperOrm.SQL/DapperRepositoryContextSQLBase.cs
+++ src/Infrastructure/MOR.Persistance.DapperOrm.SQL/DapperRepositoryContextSQLBase.cs
@@ -24,4 +24,76 @@ namespace MOR.Persistance.DapperOrm.SQL
             return new SqlConnection(ConnectionString);
         }
     }
+
+    // --------------- Example ---------------
+
+    // in your domain
+    public interface IDB2Context : IAbstractRepositoryContext
+    {
+    }
+
+    public interface ICoreContext : IAbstractRepositoryContext
+    {
+    }
+
+    public interface ICoreRepo : IAbstractRepository<ICoreContext>
+    {
+    }
+
+    public interface ICoreUserRepo : ICoreRepo
+    {
+        Task<List<string>> GetUserAsync();
+    }
+
+    public interface ICoreProductRepo : ICoreRepo
+    {
+    }
+
+
+    // in your infra
+    public class CountingRepoContext : DapperRepositoryContextSQLBase, ICoreContext
+    {
+        public CountingRepoContext(string con)
+            : base(con)
+        {
+        }
+    }
+
+    public class CoreUserRepo : DapperRepositorySQLBase<CountingRepoContext> //, ICoreUserRepo
+    {
+        public CoreUserRepo(CountingRepoContext context)
+            : base(context)
+        {
+        }
+
+
+        public async Task<List<string>> GetUserAsync()
+        {
+            var paramList = RepoContext.NewDbParams();
+
+            paramList.Int("id", 1);
+            paramList.Date("dt", DateTime.UtcNow);
+
+            var ret = await RepoContext.QueryAsync<string>("", param: paramList.ToProviderParams());
+            return ret;
+        }
+    }
+
+
+    public class MyApp
+    {
+        private readonly ICoreContext Context;
+
+        public MyApp(ICoreContext context)
+        {
+            Context = context;
+        }
+
+        public void DoJob()
+        {
+            //var repoUser = Context.GetRepository<ICoreUserRepo>();
+
+            //repoUser.GetUserAsync().GetAwaitResult();
+        }
+    }
 }
