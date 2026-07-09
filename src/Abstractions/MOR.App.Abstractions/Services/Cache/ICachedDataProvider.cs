0b30d07cee554d8df7c7e877b0685930be87f492
Ajay MS <ajay.ms2000@gmail.com>
2026-07-09 12:28:24 +0530

    AA

---
+++ src/Abstractions/MOR.App.Abstractions/Services/Cache/ICachedDataProvider.cs
@@ -0,0 +1,19 @@
+﻿namespace MOR.App.Services.Cache
+{
+    public enum CacheType
+    {
+        Local,
+        Distributed,
+    }
+
+    public interface ICachedDataProvider
+    {
+        CacheType Type { get; }
+
+        bool ContainsKey(string key);
+        Task<bool> ContainsKeyAsync(string key);
+
+        Task<T> GetOrSetObjectAsync<T>(string key, Func<Task<T>> dataProvider);
+        Task RemoveAsync(string key);
+    }
+}
